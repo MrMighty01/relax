39907bf617428f231cf9e119c7d359ff0aa1ad4c
MrMighty01 <abinavrvsa01@gmail.com>
2023-05-15 19:56:28 +0530

    pause and resume

--- pause.sh
+++ pause.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+id=$(pgrep -f "relax.sh")
+
+state=$(ps -p $id -o state=) 
+
+if [ "$state" == "T" ]; then 
+	kill -SIGCONT $id 
+	echo "The script has been Resumed"
+else 
+	kill -SIGSTOP $id
+	echo "The script has been Paused"
+fi
