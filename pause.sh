abde68b781de7bbfd0ad67839052a19f57e969e2
MrMighty01 <abinavrvsa01@gmail.com>
2023-05-18 21:29:15 +0530

    idk

--- pause.sh
+++ pause.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 id=$(pgrep -f "relax.sh")
+idmon=$(pgrep -f "moni.sh")
 
 current_state=$(ps -p "$id" -o state=) 
 
 if [ "$current_state" == "T" ]; then 
-	kill -SIGCONT $id 
-	echo "The script has been Resumed"
+	kill -SIGCONT "$idmon"
+	kill -SIGCONT "$id" 
+#echo "The script has been Resumed"
+	notify-send "The script has been Resumed"
 else 
-	kill -SIGSTOP $id
-	echo "The script has been Paused"
+	kill -SIGSTOP "$idmon"
+	PID=$(pgrep -f "paplay .*/yt/relax2020/completed/convey.wav")
+	kill "$PID"
+
+	kill -SIGSTOP "$id"
+#echo "The script has been Paused"
+	notify-send "The script has been Paused" 
 fi
--- relax.sh
+++ relax.sh
@@ -3,6 +3,6 @@
 while true; do 
 	sleep 1200  
 	notify-send "Hey Buddy, it has been 20 minutes since you sat in front of your monitor, come on, move away from your workstation, and relax your eyes by looking at something, that is 20 meters away, for at least 20 seconds :)"
-	paplay /home/abinav/yt/2020/completed/convey.wav
+	paplay /home/abinav/yt/relax2020/completed/convey.wav
 	sleep 30
 done
