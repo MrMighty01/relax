b22368377d2ddc3daf49005f7c98d11d1b9cd36e
MrMighty01 <abinavrvsa01@gmail.com>
2023-06-09 13:59:32 +0530

    small change in message

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-loc_relax=$(find ~/ -iname relax.sh -print -quit)
-loc_moni=$(find ~/ -iname moni.sh -print -quit)
--- relax.sh
+++ relax.sh
@@ -4,7 +4,7 @@ audio=$(find ~/ -iname convey.wav -print -quit)
 
 while true; do 
 	sleep 1200  
-	notify-send "Hey Buddy, it has been 20 minutes since you sat in front of your monitor, come on, move away from your workstation, and relax your eyes by looking at something, that is 20 meters away, for at least 20 seconds :)"
+	notify-send "Hey Buddy, it has been 20 minutes since you sat in front of your monitor, come on, move away from your workstation, and relax your eyes by starring at something, that is 20 feet away, for at least 20 seconds :)"
 	paplay "$audio"
 	sleep 30
 done
